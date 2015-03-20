8a54e27fdf7bebd91a70211f16ea3f916defca13
Arthur Park <arthur.park@eventure.com>
2015-03-20 15:09:47 -0700

    Yet another try to make heroku work

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var APP_ROOT_PATH = __dirname + '/ ./public';
+var APP_ROOT_PATH = __dirname + '/../public';
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(APP_ROOT_PATH));
