4b777da090c299e937af2dc7f35e5ee39b712093
Arthur Park <arthur.park@eventure.com>
2015-03-20 14:54:49 -0700

    Fixed server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
+var APP_ROOT_PATH = __dirname + '/../public';
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '../public'));
+app.use(express.static(APP_ROOT_PATH));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function(req, res) {
+  res.sendFile("index.html", {"root": APP_ROOT_PATH});
 });
 
 app.listen(app.get('port'), function() {
